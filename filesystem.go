b7b4ad2b18ae72af2a95958d80eb04a7a8c833b5
Flc゛ <four_leaf_clover@foxmail.com>
2022-05-13 18:22:33 +0800

    add workflows

--- filesystem.go
+++ filesystem.go
@@ -5,12 +5,14 @@ import (
 	"os"
 )
 
+// Exists checks if a file or directory exists
 func Exists(path string) bool {
 	_, err := Stat(path)
 
 	return err == nil
 }
 
+// Size file size
 func Size(path string) int64 {
 	info, err := Stat(path)
 
@@ -21,6 +23,7 @@ func Size(path string) int64 {
 	return info.Size()
 }
 
+// IsDir checks if a path is a directory
 func IsDir(path string) bool {
 	info, err := Stat(path)
 
@@ -31,30 +34,47 @@ func IsDir(path string) bool {
 	return info.IsDir()
 }
 
+// IsFile checks if a path is a file
 func IsFile(path string) bool {
 	return !IsDir(path)
 }
 
+// FileInfo file info, alias Stat
 func FileInfo(path string) (os.FileInfo, error) {
 	return Stat(path)
 }
 
+// Info file info, alias Stat
 func Info(path string) (os.FileInfo, error) {
 	return Stat(path)
 }
 
+// Stat file info
 func Stat(path string) (os.FileInfo, error) {
 	return os.Stat(path)
 }
 
+// ScanDir scan directory, alias ReadDir
 func ScanDir(path string) ([]os.FileInfo, error) {
 	return ReadDir(path)
 }
 
+// ReadDir read directory
 func ReadDir(path string) ([]os.FileInfo, error) {
 	return ioutil.ReadDir(path)
 }
 
+// Get get file content, alias ReadFile
+func Get(path string) (string, error) {
+	return ReadFile(path)
+}
+
+// Read read file content, alias ReadFile
+func Read(path string) (string, error) {
+	return ReadFile(path)
+}
+
+// ReadFile read file content
 func ReadFile(path string) (string, error) {
 	content, err := ioutil.ReadFile(path)
 
